42a92881108179f7f04ddcaf2d0c96daf5cf9af7
uikoo9 <uikoo9@qq.com>
2015-09-02 15:17:07 +0800

    ..

--- qiao/qiao.js
+++ qiao/qiao.js
@@ -4,6 +4,7 @@
  * 1.qiao.on
  * 2.qiao.ajax
  * 3.qiao.is
+ * 4.qiao.totop
  */
 var qiao = {};
 
@@ -84,3 +85,70 @@ qiao.totop = function(el){
 		}
 	});
 };
+
+/**
+ * qiao.qrcode
+ * 生成二维码
+ * text：待生成文字
+ * type：中文还是英文，cn为中文
+ * render：展示方式，table为表格方式
+ * width：宽度
+ * height：高度
+ * 可以参考：plugins/_03_qrcode/qrcode.html
+ */
+qiao.qrcode = function(el, options){
+	if(options){
+		var opt = {};
+		if(typeof options == 'string'){
+			opt.text = options;
+		}else{
+			if(options.text) opt.text = options.text;
+			if(options.type && options.type == 'ch') opt.text = qiao.qrcodetochar(opt.text);
+			if(options.render && options.render == 'table') opt.render = options.render;
+			if(options.width) opt.width = options.width;
+			if(options.height) opt.height = options.height;
+		}
+
+		$(el).qrcode(opt);
+	}
+	
+};
+qiao.qrcodetochar = function(str){
+    var out, i, len, c;
+    out = "";
+    len = str.length;
+    for (i = 0; i < len; i++) {
+        c = str.charCodeAt(i);
+        if ((c >= 0x0001) && (c <= 0x007F)) {
+            out += str.charAt(i);
+        } else if (c > 0x07FF) {
+            out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+            out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));
+            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+        } else {
+            out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));
+            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+        }
+    }
+    return out;
+};
+
+/**
+ * qiao.end
+ * 到达页面底部后自动加载内容
+ * end：到达底部后的回调函数
+ * $d：容器，默认是$(window)
+ * $c：内容，默认是$(document)
+ * 可以参考：plugins/_04_qend/qend.html
+ */
+qiao.end = function(end, $d, $c){
+	if(end){
+		var $d = $d || $(window);
+		var $c = $c || $(document);
+		
+		$d.scroll(function(){if($d.scrollTop() + $d.height() >= $c.height()) end();});
+	}else{
+		$(window).scroll(null);
+	}
+};
+
