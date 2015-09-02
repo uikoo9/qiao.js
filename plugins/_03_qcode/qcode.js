e49661c46971aa6610ea5837965ff55d8f646931
uikoo9 <uikoo9@qq.com>
2015-09-02 14:03:17 +0800

    qcode

---
+++ plugins/_03_qcode/qcode.js
@@ -0,0 +1,46 @@
+/**
+ * qcode
+ * 生成二维码
+ * text：待生成文字
+ * type：中文还是英文，cn为中文
+ * render：展示方式，table为表格方式
+ * width：宽度
+ * height：高度
+ */
+$.fn.qcode = function(options){
+	if(options){
+		var opt = {};
+		if(typeof options == 'string'){
+			opt.text = options;
+		}else{
+			if(options.text) opt.text = options.text;
+			if(options.type && options.type == 'ch') opt.text = $.qcodetochar(opt.text);
+			if(options.render && options.render == 'table') opt.render = options.render;
+			if(options.width) opt.width = options.width;
+			if(options.height) opt.height = options.height;
+		}
+
+		$(this).qrcode(opt);
+	}
+};
+
+// 中文的转码
+$.qcodetochar = function(str){
+    var out, i, len, c;
+    out = "";
+    len = str.length;
+    for (i = 0; i < len; i++) {
+        c = str.charCodeAt(i);
+        if ((c >= 0x0001) && (c <= 0x007F)) {
+            out += str.charAt(i);
+        } else if (c > 0x07FF) {
+            out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+            out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));
+            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+        } else {
+            out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));
+            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+        }
+    }
+    return out;
+};
--- qiao/qiao.o.js
+++ qiao/qiao.o.js
@@ -1,4 +1,27 @@
 /** 
+ * qtotop
+ * 返回顶部
+ * 可以参考：plugins/_01_qtotop/qtotop.html
+ */
+$.fn.qtotop = function(options) {
+	var $this = $(this);
+	$this.hide().click(function(){
+		$("body, html").animate({
+			scrollTop : "0px"
+		});
+	});
+	
+	var $window = $(window);
+	$window.scroll(function(){
+		if ($window.scrollTop()>0){
+			$this.fadeIn();
+		}else{
+			$this.fadeOut();
+		}
+	});
+};
+
+/**
  * 生成二维码
  * text：待生成文字
  * type：中文还是英文，cn为中文
@@ -22,27 +45,6 @@ $.fn.qcode = function(options){
 
 		$(this).qrcode(opt);
 	}
-};
-
-/** 
- * 返回顶部方法
- */
-$.fn.qtotop = function(options) {
-	var $this = $(this);
-	$this.hide().click(function(){
-		$("body, html").animate({
-			scrollTop : "0px"
-		});
-	});
-	
-	var $window = $(window);
-	$window.scroll(function(){
-		if ($window.scrollTop()>0){
-			$this.fadeIn();
-		}else{
-			$this.fadeOut();
-		}
-	});
 };
 
     exports.tip = function(func){
