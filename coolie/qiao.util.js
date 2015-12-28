d9b6059955ed57bf5bcd82aae05fdab73a142c00
uikoo9 <uikoo9@qq.com>
2015-12-28 17:34:21 +0800

    添加微信下修改title的方法， 添加cross domain ajax

--- coolie/qiao.util.js
+++ coolie/qiao.util.js
@@ -5,12 +5,14 @@
  * 3.qiao.on
  * 4.qiao.is
  * 5.qiao.ajax
- * 6.qiao.totop
- * 7.qiao.end
- * 8.qiao.cookie
- * 9.qiao.search
- * 10.qiao.juicer 
- * 11.qiao.rem
+ * 6.qiao.cajax
+ * 7.qiao.search
+ * 8.qiao.end
+ * 9.qiao.totop
+ * 10.qiao.title
+ * 11.qiao.cookie
+ * 12.qiao.juicer 
+ * 13.qiao.rem
  * @author qiaowenbin
  * @version 0.1.0.20151217
  * @history
@@ -112,66 +114,13 @@ define(function (require, exports, module) {
 	};
 	
 	/**
-	 * qiao.totop
-	 * 返回顶部的方法
-	 * 可以参考：plugins/_01_qtotop/qtotop.html
+	 * qiao.cajax
+	 * cross domain ajax
 	 */
-	exports.totop = function(el){
-		var $el = $(el);
-		$el.hide().click(function(){
-			$('body, html').animate({scrollTop : '0px'});
-		});
-		
-		var $window = $(window);
-		$window.scroll(function(){
-			if ($window.scrollTop()>0){
-				$el.fadeIn();
-			}else{
-				$el.fadeOut();
-			}
-		});
-	};
-	
-	/**
-	 * qiao.end
-	 * 到达页面底部后自动加载内容
-	 * end：到达底部后的回调函数
-	 * $d：容器，默认是$(window)
-	 * $c：内容，默认是$(document)
-	 * 可以参考：plugins/_04_qend/qend.html
-	 */
-	exports.end = function(end, $d, $c){
-		if(end){
-			var $d = $d || $(window);
-			var $c = $c || $(document);
-			
-			$d.scroll(function(){if($d.scrollTop() + $d.height() >= $c.height()) end();});
-		}else{
-			$(window).scroll(null);
-		}
-	};
-	
-	/**
-	 * qiao.cookie
-	 * 对jquery.cookie.js稍作封装
-	 * 注：需要引入jquery.cookie.js，<script src="http://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
-	 * qiao.cookie(key)：返回key对应的value
-	 * qiao.cookie(key, null)： 删除key对应的cookie
-	 * qiao.cookie(key, value)：设置key-value的cookie
-	 * 可以参考：plugins/_05_qcookie/qcookie.html
-	 */
-	exports.cookie = function(key, value, domain){
-		if(typeof value == 'undefined'){
-			return $.cookie(key);
-		}else{
-			var opt = {
-				path : '/',
-				expires : (value == null) ? -1 : 1
-			};
-			if(domain) opt.domain = domain;
-			
-			$.cookie(key, value, opt);
-		}
+	exports.ajax = function(options, suc, fail){
+		var opt = exports.ajaxopt(options);
+		opt.crossDomain = true;
+		exports.ajax(opt, suc, fail);
 	};
 	
 	/**
@@ -196,6 +145,80 @@ define(function (require, exports, module) {
 		}
 		
 		return res;
+	};
+	
+	/**
+	 * qiao.end
+	 * 到达页面底部后自动加载内容
+	 * end：到达底部后的回调函数
+	 * $d：容器，默认是$(window)
+	 * $c：内容，默认是$(document)
+	 * 可以参考：plugins/_04_qend/qend.html
+	 */
+	exports.end = function(end, $d, $c){
+		if(end){
+			var $d = $d || $(window);
+			var $c = $c || $(document);
+			
+			$d.scroll(function(){if($d.scrollTop() + $d.height() >= $c.height()) end();});
+		}else{
+			$(window).scroll(null);
+		}
+	};
+	
+	/**
+	 * qiao.totop
+	 * 返回顶部的方法
+	 * 可以参考：plugins/_01_qtotop/qtotop.html
+	 */
+	exports.totop = function(el){
+		var $el = $(el);
+		$el.hide().click(function(){
+			$('body, html').animate({scrollTop : '0px'});
+		});
+		
+		var $window = $(window);
+		$window.scroll(function(){
+			if ($window.scrollTop()>0){
+				$el.fadeIn();
+			}else{
+				$el.fadeOut();
+			}
+		});
+	};
+	
+	/**
+     * 动态设置title
+     */
+    exports.title = function(t){
+    	if(t){
+    		$('title').text(t);
+    		var $iframe = $('<iframe src="http://www.baidu.com/img/baidu_jgylogo3.gif" style="display:none;"></iframe>');
+    		$iframe.on('load', function(){setTimeout(function(){$iframe.off('load').remove();}, 0);}).appendTo($('body'));
+    	}
+    };
+	
+	/**
+	 * qiao.cookie
+	 * 对jquery.cookie.js稍作封装
+	 * 注：需要引入jquery.cookie.js，<script src="http://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+	 * qiao.cookie(key)：返回key对应的value
+	 * qiao.cookie(key, null)： 删除key对应的cookie
+	 * qiao.cookie(key, value)：设置key-value的cookie
+	 * 可以参考：plugins/_05_qcookie/qcookie.html
+	 */
+	exports.cookie = function(key, value, domain){
+		if(typeof value == 'undefined'){
+			return $.cookie(key);
+		}else{
+			var opt = {
+				path : '/',
+				expires : (value == null) ? -1 : 1
+			};
+			if(domain) opt.domain = domain;
+			
+			$.cookie(key, value, opt);
+		}
 	};
 	
 	/**
